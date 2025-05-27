43a57079a2a26ebed395e7bd3a7c8a715cc84a3a
Simon <simonsunuwar12@gmail.com>
2025-05-27 15:06:02 -0600

    1. Added a functional side menu bar 2. Removed completed list from homeview 3. Created completedlistview so i have another page destination to test side menu

---
+++ MiniMe/Views/SideMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuView.swift
+//  MiniMe
+//
+//  Created by Simon on 2025-05-27.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuView()
+}
